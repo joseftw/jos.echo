82ec0c1196e6b4c3899e26a6099eb91a6308eebd
joseftw <josef@localhost.local>
2023-09-07 10:50:33 +0200

    Assembly versions

--- src/JOS.Echo/EchoRequestHandler.cs
+++ src/JOS.Echo/EchoRequestHandler.cs
@@ -1,7 +1,22 @@
+using System.Reflection;
+
 namespace JOS.Echo;
 
 public static class EchoRequestHandler
 {
+    private static readonly string Assembly;
+    private static readonly string FileVersion;
+    private static readonly string InformationalVersion;
+
+    static EchoRequestHandler()
+    {
+        var assembly = typeof(Program).Assembly;
+
+        Assembly = assembly.GetCustomAttribute<AssemblyVersionAttribute>()!.Version;
+        FileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()!.Version;
+        InformationalVersion =
+            assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+    }
     public static IResult Handle(HttpContext httpContext, MountedCertificateReader mountedCertificateReader)
     {
         var certificate = mountedCertificateReader.Read();
@@ -9,22 +24,22 @@ public static class EchoRequestHandler
         {
             Version = new
             {
-                Assembly = ThisAssembly.AssemblyVersion,
-                FileVersion = ThisAssembly.AssemblyFileVersion,
-                Informational = ThisAssembly.AssemblyInformationalVersion
+                Assembly,
+                FileVersion,
+                InformationalVersion
             },
             Certificate = new
             {
-                NotBefore = certificate?.NotBefore,
-                NotAfter = certificate?.NotAfter,
-                SerialNumber = certificate?.SerialNumber,
+                certificate?.NotBefore,
+                certificate?.NotAfter,
+                certificate?.SerialNumber,
                 Subject = certificate?.SubjectName.Name,
-                Thumbprint = certificate?.Thumbprint
+                certificate?.Thumbprint
             },
             Request = new
             {
                 Headers = httpContext.Request.Headers.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value),
-                Protocol = httpContext.Request.Protocol
+                httpContext.Request.Protocol
             }
         });
     }
