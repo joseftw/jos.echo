227b8973f88f194a4093dabc5e7fb73466ba5cf4
joseftw <josef@localhost.local>
2024-11-04 15:14:50 +0100

    Formatting

--- src/JOS.Echo/OpenTelemetry/OpenTelemetryWebApplicationBuilderExtensions.cs
+++ src/JOS.Echo/OpenTelemetry/OpenTelemetryWebApplicationBuilderExtensions.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace JOS.Echo.OpenTelemetry;
@@ -12,15 +13,16 @@ public static class OpenTelemetryWebApplicationBuilderExtensions
         this WebApplicationBuilder builder, OpenTelemetryConfiguration openTelemetryConfiguration)
     {
         var openTelemetryBuilder = builder.Services.AddOpenTelemetry();
-
+        openTelemetryBuilder.ConfigureResource(b => { b.AddService("JOS.Echo"); });
         if (openTelemetryConfiguration.Logging.Enabled)
         {
-            openTelemetryBuilder = openTelemetryBuilder.WithLogging(l =>
+            openTelemetryBuilder.WithLogging(l =>
             {
                 if (openTelemetryConfiguration.Logging.ConsoleEnabled)
                 {
                     l.AddConsoleExporter();
                 }
+
                 if (openTelemetryConfiguration.Logging.OLTPEnabled)
                 {
                     l.AddOtlpExporter(options =>
@@ -33,7 +35,7 @@ public static class OpenTelemetryWebApplicationBuilderExtensions
 
         if (openTelemetryConfiguration.Metrics.Enabled)
         {
-            openTelemetryBuilder = openTelemetryBuilder.WithMetrics(m =>
+            openTelemetryBuilder.WithMetrics(m =>
             {
                 m.AddAspNetCoreInstrumentation();
                 m.AddRuntimeInstrumentation();
@@ -42,6 +44,7 @@ public static class OpenTelemetryWebApplicationBuilderExtensions
                 {
                     m.AddConsoleExporter();
                 }
+
                 if (openTelemetryConfiguration.Metrics.OLTPEnabled)
                 {
                     m.AddOtlpExporter(options =>
@@ -54,7 +57,7 @@ public static class OpenTelemetryWebApplicationBuilderExtensions
 
         if (openTelemetryConfiguration.Tracing.Enabled)
         {
-            openTelemetryBuilder = openTelemetryBuilder.WithTracing(t =>
+            openTelemetryBuilder.WithTracing(t =>
             {
                 t.AddAspNetCoreInstrumentation();
                 t.AddHttpClientInstrumentation();
@@ -62,6 +65,7 @@ public static class OpenTelemetryWebApplicationBuilderExtensions
                 {
                     t.AddConsoleExporter();
                 }
+
                 if (openTelemetryConfiguration.Tracing.OLTPEnabled)
                 {
                     t.AddOtlpExporter(options =>
